ec1d4208a7eb834f0b05a9155900f2d3d3c89551
Diegokh <diegoblanque04@outlook.com>
2024-11-07 14:46:58 +0100

    Versión 2.1

---
+++ php_gestionInvernaderos/agrosmart.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>AgroSmart</h1>
+    </header>
+
+    <div>
+        <a href="./sensores_invernadero.php">Consultar Sensores en invernadero</a>
+        <a href="#">Consultar Dueños de cada invernadero</a>
+        <a href="#">Consultar Historial de control</a>
+        <a href="#">Consultar Alertas</a>
+        <a href="#">Consultar Lecturas de sensores</a>
+        <a href="#">Gestionar Usuarios</a>
+        <a href="#">Gestionar Invernaderos</a>
+    </div>
+    
+    
+</body>
+</html>
